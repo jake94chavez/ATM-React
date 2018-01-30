e656d7bb0cfece2e614f7ad2dea7c38c3b9999ab
Jake Chavez <jake94chavez@gmail.com>
2018-01-29 21:50:06 -0600

    fixes NaN bug

--- src/Account.js
+++ src/Account.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 export default class Account extends Component {
   constructor (props) {
     super(props)
@@ -10,8 +13,9 @@ export default class Account extends Component {
   handleDepositClick(e) {
     e.preventDefault();
     let amount = Math.round(parseFloat(this.inputBox.value)*100)/100;
+    !isNaN(amount) ? amount = Math.round(parseFloat(this.inputBox.value) * 100)/100 : amount = 0;
     let newBalance = this.state.balance + amount;
-    if (amount < 0) {
+     if (amount < 0) {
       alert('We cannot deposit money you don\'t have!')
     } else {
     this.setState({
@@ -38,7 +42,7 @@ export default class Account extends Component {
       <div className="account">
         <h2>{this.props.name}</h2>
         <div className="balance">{this.state.balance}</div>
-        <input type="number" placeholder="enter an amount" ref={(input) => this.inputBox = input}/>
+        <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input}/>
         <input type="button" value="Deposit" onClick={(e) => this.handleDepositClick(e)} />
         <input type="button" value="Withdraw" onClick={(e) => this.handleWithdrawClick(e)} />
       </div>
